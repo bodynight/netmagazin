0c559a03ba2134ae4f3476174db4d1ab055afbc6
bodynight <xbodynightx@gmail.com>
2021-06-14 18:33:55 +0300

    apdate add_to_cart function

--- public/app.js
+++ public/app.js
@@ -9,5 +9,8 @@ function aaa()
 
 function add_to_cart(id)
 {
-	alert('You added pizza whith id:' + id);
-}	
+	var key = 'product_' + id;
+var x = window.localStorage.getItem(key);
+x = x*1 + 1;
+window.localStorage.setItem(key, x);
+}
