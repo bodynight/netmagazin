1393d574ccbf8f6488f84473ae913e5f309403bc
bodynight <xbodynightx@gmail.com>
2021-06-15 19:06:45 +0300

    update orders input field

--- public/app.js
+++ public/app.js
@@ -7,14 +7,19 @@ function aaa()
 		alert(x);
 	}
 
+function update_orders_input()
+{
+	var orders = cart_get_orders();
+	$('#orders_input').val(orders);
+}	
+
 function add_to_cart(id)
 {
 	var key = 'product_' + id;
 var x = window.localStorage.getItem(key);
 x = x*1 + 1;
 window.localStorage.setItem(key, x);
-
-alert ('Items in you cart: ' + cart_get_number_of_items());
+update_orders_input()
 }
 
 function cart_get_number_of_items()
@@ -30,4 +35,19 @@ for(var i = 0; i < window.localStorage.length; i++)
 			}
 	}
 	return cnt;
+}
+
+function cart_get_orders()
+{
+	var orders = '';
+for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i);
+		var value = window.localStorage.getItem(key);
+		if(key.indexOf('product_') == 0)
+			{
+				orders = orders + key + '=' +value + ', ';
+			}
+	}
+	return orders;
 }
