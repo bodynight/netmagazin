b56d410d340994da8dbb93e4831a6f91a8ad6b98
bodynight <xbodynightx@gmail.com>
2021-06-15 22:56:44 +0300

    update button cart(...) in nav bar

--- public/app.js
+++ public/app.js
@@ -13,13 +13,20 @@ function update_orders_input()
 	$('#orders_input').val(orders);
 }	
 
+function update_orders_button()
+{
+	var text = 'Cart (' + cart_get_number_of_items() + ')';
+	$('#orders_button').val(text);
+}
+
 function add_to_cart(id)
 {
 	var key = 'product_' + id;
 var x = window.localStorage.getItem(key);
 x = x*1 + 1;
 window.localStorage.setItem(key, x);
-update_orders_input()
+update_orders_input();
+update_orders_button();
 }
 
 function cart_get_number_of_items()
