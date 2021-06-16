1a632c7eddddf0ffc38299d601b42930eee4897f
bodynight <xbodynightx@gmail.com>
2021-06-16 12:39:40 +0300

    add function str_tohash

--- app.rb
+++ app.rb
@@ -7,8 +7,21 @@ require 'sinatra/activerecord'
 set :database, {adapter:"sqlite3", database:"pizzashop.db"}
 
 class Product < ActiveRecord::Base
-
 end
+
+def str_tohash
+	@input =	@order.delete('product_')
+	@arr = @input.split(/[=,]/)
+	@order_h = Hash.new
+		@arr.each_slice(2) do |k, v|
+			if  v != nil
+			key = k.strip.to_i
+			value = v.strip.to_i
+			@order_h[key] = value
+			end
+		end
+end
+
 
 get '/' do
 	@product = Product.all
@@ -16,7 +29,9 @@ get '/' do
 end
 
 post '/cart' do
- erb "Hello World"
+	@order = params[:order]
+	str_tohash	
+	erb :cart
 end
 
 get '/about' do
